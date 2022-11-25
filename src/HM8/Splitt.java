51ac04835c9b8304cfe7c494732b07d5e3fe373b
nikita-qwerty123 <nikitos.barca@gmail.com>
2022-11-25 17:55:57 +0200

    HM8 update2

--- src/HM8/Splitt.java
+++ src/HM8/Splitt.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class splitt {
+public class Splitt {
 
     public static void main(String[] args) {
         String line = "cat, dog, rabbit, cat, turtle, rabbit, humster, dog";
@@ -16,13 +16,8 @@ public class splitt {
         String[] words = line.split(", ");
         Set<String> list = new HashSet<>(Arrays.asList(words));
         for (final String s : list) {
-            System.out.println(firstUpperCase(s));
+            System.out.println(s.substring(0, 1).toUpperCase() + s.substring(1));
         }
-    }
-
-    public static String firstUpperCase(String word) {
-        if (word == null || word.isEmpty()) return ""; //или return word;
-        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 }
 
